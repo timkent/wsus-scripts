494aec6f88945616a3126ecf7fb0b6e0ea43bd5c
Tim Kent <tim@kent.id.au>
2017-05-16 13:20:34 +1000

    Create WSUS-Approve.ps1

---
+++ WSUS-Approve.ps1
@@ -0,0 +1,14 @@
+[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer();
+ 
+# Accept EULA for all updates
+$updates = $wsus.GetUpdates()
+$license = $updates | Where {$_.RequiresLicenseAgreementAcceptance}
+$license | ForEach {$_.AcceptLicenseAgreement()}
+ 
+# Accept for All Computers and subgroups
+$group = $wsus.GetComputerTargetGroups() | Where-Object {$_.Name -Eq "All Computers"}
+ 
+$updates | Where-Object {($_.UpdateClassificationTitle -Eq "Critical Updates" -Or $_.UpdateClassificationTitle -Eq "Security Updates" -Or $_.UpdateClassificationTitle -Eq "Service Packs" -Or $_.UpdateClassificationTitle -Eq "Update Rollups") -And (-Not $_.IsDeclined)} | ForEach {
+    $_.Approve("Install", $group)
+}
