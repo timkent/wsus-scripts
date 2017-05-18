60b9179becfbb003e1ed5f6a8ceecb62ccf04328
Tim Kent <tim@kent.id.au>
2017-05-18 16:46:56 +1000

    Improve error handling

--- WSUS-Approve.ps1
+++ WSUS-Approve.ps1
@@ -1,14 +1,30 @@
-[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null
-$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer();
- 
+# WSUS-Approve.ps1
+# https://github.com/timkent/wsus-scripts
+
+try {
+    [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | Out-Null
+    $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer()
+}
+catch {
+    throw "Problem running GetUpdateServer()"
+}
+
+try {
+    $updates = $wsus.GetUpdates()
+}
+catch {
+    throw "Problem running GetUpdates()"
+}
+
 # Accept EULA for all updates
-$updates = $wsus.GetUpdates()
-$license = $updates | Where-Object {$_.RequiresLicenseAgreementAcceptance}
-$license | ForEach-Object {$_.AcceptLicenseAgreement()}
- 
+if ($license = $updates | Where-Object {$_.RequiresLicenseAgreementAcceptance}) {
+    $license | ForEach-Object {$_.AcceptLicenseAgreement()}
+}
+
 # Accept for All Computers and subgroups
-$group = $wsus.GetComputerTargetGroups() | Where-Object {$_.Name -Eq "All Computers"}
- 
-$updates | Where-Object {($_.UpdateClassificationTitle -Eq "Critical Updates" -Or $_.UpdateClassificationTitle -Eq "Security Updates" -Or $_.UpdateClassificationTitle -Eq "Service Packs" -Or $_.UpdateClassificationTitle -Eq "Update Rollups") -And (-Not $_.IsDeclined)} | ForEach {
-    $_.Approve("Install", $group)
+if ($group = $wsus.GetComputerTargetGroups() | Where-Object {$_.Name -Eq "All Computers"}) {
+    # Unfortunately we can't use (-Not $_.IsApproved) here as it is set when the update is approved in any group/subgroup
+    $updates | Where-Object {($_.UpdateClassificationTitle -Eq "Critical Updates" -Or $_.UpdateClassificationTitle -Eq "Security Updates" -Or $_.UpdateClassificationTitle -Eq "Service Packs" -Or $_.UpdateClassificationTitle -Eq "Update Rollups") -And (-Not $_.IsDeclined)} | ForEach-Object {
+        $_.Approve("Install", $group) | Out-Null
+    }
 }
