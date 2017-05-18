91be488669e55fa08bf929a56d3d0950e4fffa22
Tim Kent <tim@kent.id.au>
2017-05-18 16:47:27 +1000

    Improve error handling

--- WSUS-Maintenance.ps1
+++ WSUS-Maintenance.ps1
@@ -1,19 +1,37 @@
-[reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | out-null
-$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer();
+# WSUS-Maintenance.ps1
+# https://github.com/timkent/wsus-scripts
+
+try {
+    [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | Out-Null
+    $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::GetUpdateServer()
+}
+catch {
+    throw "Problem running GetUpdateServer()"
+}
+
+try {
+    $updates = $wsus.GetUpdates()
+}
+catch {
+    throw "Problem running GetUpdates()"
+}
 
 # Accept EULA for all updates
-$updates = $wsus.GetUpdates()
-$license = $updates | Where-Object {$_.RequiresLicenseAgreementAcceptance}
-$license | ForEach-Object {$_.AcceptLicenseAgreement()}
+if ($license = $updates | Where-Object {$_.RequiresLicenseAgreementAcceptance}) {
+    $license | ForEach-Object {$_.AcceptLicenseAgreement()}
+}
+
 Remove-Variable license, updates
 
 # WSUS maintenance tasks
-$cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope;
-$cleanupScope.DeclineSupersededUpdates = $true
-$cleanupScope.DeclineExpiredUpdates = $true
-$cleanupScope.CleanupObsoleteUpdates = $true
-$cleanupScope.CompressUpdates = $true
-#$cleanupScope.CleanupObsoleteComputers = $true
-$cleanupScope.CleanupUnneededContentFiles = $true
-$cleanupManager = $wsus.GetCleanupManager();
-$cleanupManager.PerformCleanup($cleanupScope);
+if ($cleanupScope = new-object Microsoft.UpdateServices.Administration.CleanupScope) {
+    $cleanupScope.DeclineSupersededUpdates = $true
+    $cleanupScope.DeclineExpiredUpdates = $true
+    $cleanupScope.CleanupObsoleteUpdates = $true
+    $cleanupScope.CompressUpdates = $true
+    $cleanupScope.CleanupObsoleteComputers = $true
+    $cleanupScope.CleanupUnneededContentFiles = $true
+    if ($cleanupManager = $wsus.GetCleanupManager()) {
+        $cleanupManager.PerformCleanup($cleanupScope)
+    }
+}
